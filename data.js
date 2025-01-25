f1e70586360ddf60117b3651ef309636492e862b
Xcilik <149920327+Xcilik@users.noreply.github.com>
2025-01-25 19:11:18 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "7986593846:AAHc23feoe6nSOb89037DFt8Yi_EmSzrzgY",
+  token: "7474656174:AAETmwvH4gK7D6yTJth17czHRj8VqohhXFE",
   id:"1784606556",
   address:"https://ro-isa2.onrender.com/"
 };
