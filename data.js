db2e51944462d8ae7e6a5025ebbc7339de7ce28e
Xcilik <149920327+Xcilik@users.noreply.github.com>
2025-01-25 18:49:13 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "7986593846:AAHGqY4xJr4x_brirzLVR1XBgso_b6Verxc",
+  token: "8070369541:AAE6vJ_brwmcSSojZWLkehb71sYTpDRhYps",
   id:"1784606556",
   address:"https://ro-ry7t.onrender.com/"
 };
